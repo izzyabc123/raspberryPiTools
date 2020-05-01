b1cf8776820d26af228fc7fa6d216762bbae77f4
Isabelle Vidana <isabelle.monroe@gmail.com>
2020-05-01 13:55:05 -0400

    adding nginx setup steps

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#one of the first steps is to set up vi:
+#copy the vimrc file into: /etc/vim and overwrite the vimrc file there
+
+
+#install nginx:
+sudo apt update
+sudo apt install nginx
+sudo reboot
