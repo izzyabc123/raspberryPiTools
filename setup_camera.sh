a770d9ee9e43025ec6794ec66cc942a5059dcc3b
Isabelle Vidana <isabelle.monroe@gmail.com>
2020-05-01 17:17:36 -0400

    details

--- setup_camera.sh
+++ setup_camera.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-apt-cache search v4l2
-sudo apt install fswebcam libv4l-dev v4l-utils ffmpeg
+##install tools:
+#apt-cache search v4l2
+#sudo apt install fswebcam libv4l-dev v4l-utils ffmpeg
+
+#from here, use the example config file to develop use case for fsweb camera app
 
