bf28fc920d9e3613566c8471d0e157448a5bb6e5
Isabelle Vidana <isabelle.monroe@gmail.com>
2020-05-01 16:16:43 -0400

    setup added

--- setup.sh
+++ setup.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-#one of the first steps is to set up vi:
-#copy the vimrc file into: /etc/vim and overwrite the vimrc file there
+##one of the first steps is to set up vi:
+##install vim: 
+#sudo apt install vim
+##copy the vimrc file into: /etc/vim and overwrite the vimrc file there
 
 
-#install nginx:
-sudo apt update
-sudo apt install nginx
-sudo reboot
+##install nginx:
+#sudo apt update
+#sudo apt install nginx
+#sudo reboot
+
+##check hostname on pi:
+#echo "Pi's current hostname:\n"
+#hostname -I
+#echo "Pi's current internet setup:\n"
+#sudo netstat -tlpn
+
+##check nginx config file:
+#sudo nginx -t
+
+##test nginx config file:
+#sudo service nginx configtest
+#sudo service nginx restart
+
+##config file for nginx is in /etc/nginx
+##the main config is nginx.conf
+
